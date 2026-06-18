feddbf0421578fc20651ad6cbbec63482fc0c5b4
dhekiakbar <dhekiakbar23@outlook.com>
2026-06-18 23:32:13 +0700

    UPDATE = User Management new feature(Create User, Reset Password, Delete User)

--- Backend/migrations/table_announcements.sql
+++
@@ -1,9 +0,0 @@
---Tambahkan migrasi untuk tabel announcements--
---Buat tabel announcements untuk menyimpan pengumuman di database--
--- Tabel Announcements (Pengumuman)
-CREATE TABLE announcements (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
